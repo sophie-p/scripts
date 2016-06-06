b5d1782b52812eeaf4731b0e0e066c76012fef18
Sophie Peron <speron@obs-vlfr.fr>
2016-06-06 10:50:41 +0200

    added melt temperature

--- dnacalc.py
+++ dnacalc.py
@@ -2,7 +2,11 @@
 
 # comments
 
-DNASeq = 'ATGAAC'
+# DNASeq = raw_input ("Enter a DNA sequence: ")
+
+DNASeq = 'AATCGCGATTATCGCATCGATCG'
+DNASeq = DNASeq.upper()
+
 print( 'Sequence ' + DNASeq )
 
 SeqLength = float( len( DNASeq ))
@@ -12,7 +16,22 @@ NumberA = DNASeq.count('A')
 NumberT = DNASeq.count('T')
 NumberC = DNASeq.count('C')
 NumberG = DNASeq.count('G')
-print ( 'A: ' + str( NumberA / SeqLength))
-print ( 'T: ' + str( NumberT / SeqLength))
-print ( 'C: ' + str( NumberC / SeqLength))
-print ( 'G: ' + str( NumberG / SeqLength))
+
+print 'A: {:.2f}'.format (NumberA / SeqLength)
+print 'T: {:.2f}'.format (NumberT / SeqLength)
+print 'C: {:.2f}'.format (NumberC / SeqLength)
+print 'G: {:.2f}'.format (NumberG / SeqLength)
+
+TotalStrong = NumberG + NumberC
+TotalWeak = NumberA + NumberT
+
+if SeqLength <= 14:
+	MeltTemp = (4 * TotalStrong) + (2 * TotalWeak)
+	print("Using short formula.")
+else: 
+	MeltTemp = 64.9 + 41 * (TotalStrong - 16.4) / SeqLength
+	print("Using long formula.")
+	
+print ( 'Melting temperature: {}'.format(MeltTemp))
+
+print ('Done')
